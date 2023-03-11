c577ce7e6126f0649d0979513653655f9b885043
Ashutosh Pandey <94055977+abtp2@users.noreply.github.com>
2023-03-12 00:16:35 +0530

    Add files via upload

--- Blog/index.js
+++ Blog/index.js
@@ -1,3 +1,58 @@
+/* navbar */
+  // search-box open close js code
+let navbar = document.querySelector(".navbar");
+/*  let searchBox = document.querySelector(".search-box .bx-moon"); */
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+/*  
+searchBox.addEventListener("click", ()=>{
+  navbar.classList.toggle("showInput");
+  if(navbar.classList.contains("showInput")){
+    searchBox.classList.replace("bx-moon" ,"bxs-sun");
+  }else {
+    searchBox.classList.replace("bxs-sun" ,"bx-moon");
+  }
+}); */
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+document.body.classList.add("overlay-dark");
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+document.body.classList.remove("overlay-dark");
+}
+
+document.addEventListener('mouseup', function(e) {
+var container = navLinks;
+if (!container.contains(e.target)){
+navLinks.style.left = "-100%"; 
+document.body.classList.remove("overlay-dark");
+}});	
+
+
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+
+/*  
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
+ */
 /* onload window */
 window.onload = function(){
 var y = localStorage.getItem("theme");
@@ -25,7 +80,7 @@ x.classList.replace("bxs-sun", "bx-moon");
 
 
 /* theme toggler */
-document.getElementById("theme-toggler").onclick = function(){
+document.getElementById("bx-moon").onclick = function(){
 var x = document.getElementById("bx-moon");
 var body = document.body;
 
