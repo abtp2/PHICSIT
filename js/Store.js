47f677673d2c9ee7fc7eb83188fc93b836f3e2e2
Ashutosh Pandey <94055977+abtp2@users.noreply.github.com>
2023-03-31 13:42:02 +0530

    Add files via upload

--- js/Store.js
+++ js/Store.js
@@ -130,6 +130,7 @@ localStorage.setItem("theme", "light");
 /* search bar */
 
 document.getElementById("search-toggle").onclick = () =>{
+showCards();
 slideToggle(document.querySelector("#first form") , 100, searchBack());				
 document.querySelector("#first form input").focus();
 document.querySelector("#first form input").click();
@@ -137,11 +138,13 @@ document.querySelector("#first form input").click();
 
 document.onmouseup = (e) =>{
 var container = document.querySelector("#first form");
-if(!container.contains(e.target) && !document.getElementById("search-toggle").contains(e.target) && !document.querySelector("nav").contains(e.target)){
+if(!container.contains(e.target) && !document.getElementById("search-toggle").contains(e.target) && !document.querySelector("nav").contains(e.target) && !document.getElementsByClassName("row")[0].contains(e.target)){
+showCards();
 slideUp(document.querySelector("#first form") , 100, searchBack());
 }}
 
 function searchBack(){
+showCards();
 document.querySelector("#first form input").value ="";
 }
 
@@ -238,6 +241,58 @@ elem.innerHTML =`<div class="${i}"><img src="${books[i].image}" class="${books[i
 		<button class="addcart-btn"><i class="bx bx-cart"></i>Add to cart</button></span>`;
 document.getElementsByClassName("row")[0].appendChild(elem);
 }
+
+
+
+
+
+/* filter books based on search input */
+document.getElementById("search-int").oninput=(e)=>{
+	const query = e.target.value.toLowerCase().trim().replace(/\s{2,}/g, ' ');
+	for (let i = 0; i < books.length; i++) {
+		const title = books[i].title.toLowerCase();
+		const elem = document.getElementsByClassName(`${i}`)[0].closest(".card");
+		if(e.target.value ===""){
+	 	elem.style.display = "block";
+		}
+		else{
+		   if (title.includes(query)) {
+			 elem.style.display = "block";
+			 }else{
+			 elem.style.display = "none";
+			 document.querySelector("#second h4").style.display ="block";}
+		}
+		
+ }//for loop ended
+ 
+ 
+/* Show/hide 'No matching books' message */
+const matchingCards = document.getElementsByClassName("row")[0].querySelectorAll(".card[style='display: block;']");
+if(matchingCards.length === 0){
+document.querySelector("#second h4").style.display = "block";
+} else{
+document.querySelector("#second h4").style.display = "none";} 
+}//oninput function ended
+
+
+
+
+
+
+/* function to display all the cards */
+function showCards(){
+var cards = document.getElementsByClassName("row")[0].querySelectorAll(".card");
+cards.forEach(card => {
+card.style.display ="block";
+});
+document.querySelector("#second h4").style.display = "none";
+}
+
+
+
+
+
+
 
 
 
